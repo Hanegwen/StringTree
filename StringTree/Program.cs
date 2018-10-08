3cea57791eff9edab95b5f86ebc63b157a1c8b9a
unknown <Michael.Morrone@iam.local>
2018-10-08 14:44:12 -0500

    Um

---
+++ StringTree/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StringTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree tree = new Tree();
+
+            tree.LoadText();
+
+            if(tree.IsReady())
+            {
+                //Yes It's Ready
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringTree/Tree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringTree
+{
+    class Tree
+    {
+        bool isReady = false;
+        public void LoadText()
+        {
+            string[] TheBaseData = System.IO.File.ReadAllLines(@"C:\workspace\TreeData.txt");
+
+            foreach(string data in TheBaseData)
+            {
+                
+                if(data.StartsWith(" "))
+                {
+                    Console.WriteLine(data + "1");
+                }
+                else
+                {
+                    Console.WriteLine(data);
+                }
+            }
+
+            isReady = true;
+        }
+
+        public bool IsReady()
+        {
+            return isReady;
+        }
+
+        public void GetNode(int someReference)
+        {
+
+        }
+
+        public void WriteOutLineFile()
+        {
+
+        }
+    }
+}
